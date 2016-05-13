81f82f36c91baf890bb0ad6de71595eee08913ad
luuizdias <luiz.henrique.dias@hotmail.com>
2016-05-13 12:38:39 -0300

    13/05/2016
    Commit com a aplicação gerando logfile numerado, e funcionando!!!!

--- wishbone/logipi/read_write_time.c
+++ wishbone/logipi/read_write_time.c
@@ -10,17 +10,18 @@
 #include <sys/ioctl.h>
 #include "wishbone_wrapper.h"
 
-//#define n 2048; //numero de bits
-
 int main(int argc, char ** argv){
 	int fd,address ;
 	unsigned short i ;
-	int j;
+	int j, k;
 	int bits = atoi(argv[1]);
-	unsigned short writeVal ;
+	unsigned char *writeVal ;
 	//logfile
+	char text[15] = "logfile";
+	strcat(text,argv[2]);
+	strcat(text,".csv");	
 	FILE *log;
-	log = fopen("logfile.txt","a");
+	log = fopen(text,"a");
 	
 	//leitura de tempo
 	struct timeval temp1,temp2;
@@ -32,52 +33,47 @@ int main(int argc, char ** argv){
 		return 0 ;
 	}
 
-	printf("Com n = %d",bits);
 	fprintf(log,"%d,",bits);
-	//printf("\n");
-	//printf("WRITE\n");
-	//printf("\n");
-	gettimeofday(&temp1,NULL);
-	for (j=0;j<bits;j++) { // write
-		address = (int) 0x1000 + (j<<1); //strtol(argv[1], NULL, 0);//write
-		writeVal = (unsigned short) j+1; //strtol(argv[2], NULL, 0);
-		//printf("Writing 0x%x @ 0x%x \n", writeVal, address);
-		
-		//if(wishbone_write(&writeVal, 2, address) < 2){
-			//printf("Write error !");
-		//}	
-	}
-	gettimeofday(&temp2,NULL);
-	elapsed_s_sec=temp2.tv_sec-temp1.tv_sec;
-	elapsed_u_sec=temp2.tv_usec-temp1.tv_usec;
-	elapsed_u_time=(elapsed_s_sec)*100000+elapsed_u_sec;	
-	fprintf(log,"%ld,",elapsed_u_time);
-	fprintf(log,"%d,",(2048*1000)/elapsed_u_time );
 
-	
-	//printf("\n");
-	//printf("READ\n");
-	//printf("\n");
+	//printf("WRITE\n");
+	writeVal = (unsigned char *) malloc(sizeof(unsigned short)*bits);
+	for (j=0;j<bits;j++) { // write
+		//address = (int) 0x1000 + (j<<1); //strtol(argv[1], NULL, 0);//write
+		writeVal[j] = (unsigned char) j+1; //strtol(argv[2], NULL, 0);
+		//printf("Writing 0x%x @ 0x%x \n", writeVal, address		
+	}
 	gettimeofday(&temp1,NULL);
-	for (j=0;j<bits;j++) { //read
-		address = (int) 0x1000 + (j<<1); 
-		unsigned short readVal ; //read
-		
-		wishbone_read(&readVal,2,address);
-		//printf("0x%x \n",readVal);
-		
+	for(k=0;k<10; k++){
+		wishbone_write(writeVal, bits, (int) 0x1000);
 	}
 	gettimeofday(&temp2,NULL);
-	elapsed_s_sec=temp2.tv_sec-temp1.tv_sec;
-	elapsed_u_sec=temp2.tv_usec-temp1.tv_usec;
-	elapsed_u_time=(elapsed_s_sec)*100000+elapsed_u_sec;
+
+	elapsed_u_time = ((temp2.tv_usec-temp1.tv_usec)+(temp2.tv_sec-temp1.tv_sec)*1000000L)/10;
 	fprintf(log,"%ld,",elapsed_u_time);
-	fprintf(log,"%d,",(2048*1000)/elapsed_u_time );	
+	fprintf(log,"%f,",((float)bits)/(float)elapsed_u_time );
+	
+	//printf("READ\n");
+	
+	unsigned char *readVal = (unsigned char*) malloc(sizeof(unsigned char)*bits);
+	
+	for (j=0;j<bits;j++) { //read
+		readVal[j] = 0;		
+	}
+	gettimeofday(&temp1,NULL);
+	for(k=0;k<10; k++){
+		wishbone_read(readVal,bits,(int)0x1000);
+	}
+	gettimeofday(&temp2,NULL);
+	
+	elapsed_u_time = ((temp2.tv_usec-temp1.tv_usec)+(temp2.tv_sec-temp1.tv_sec)*1000000L)/10;
+	fprintf(log,"%ld,",elapsed_u_time);
+	fprintf(log,"%f,",((float)bits)/(float)elapsed_u_time );
 	fprintf(log,"\n");
 	
 	fclose(log);
 	close(fd);
-	free(bits);
+	free(writeVal);
+	free(readVal);
 	return 0 ;
 }
 
--- wishbone/logipi/roda.sh
+++ wishbone/logipi/roda.sh
@@ -1,8 +1,11 @@
 #/bin/bash
-
+sudo logi_loader logipi_wishbone.bit;
+for j in $(seq 1 1 10);
+do
+echo "$j" 
 for i in $(seq 2 2 2048); 
 do
 	echo "$i"
-	./read_write_time $i
-	sleep 1
+	./read_write_time $i $j
 done	
+done
